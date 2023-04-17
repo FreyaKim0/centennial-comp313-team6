13b5e5844496f453ab5c50a11aa1de5da9246851
Freya <64510536+FreyaKim0@users.noreply.github.com>
2023-04-16 21:23:28 -0400

    update mongoDB connection

--- app/src/main/java/com/example/centenarioschoolapp/StudentRegistrationStatus.java
+++ app/src/main/java/com/example/centenarioschoolapp/StudentRegistrationStatus.java
@@ -7,76 +7,66 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
-
+import com.mongodb.client.MongoClient;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
-import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
 public class StudentRegistrationStatus<App> extends AppCompatActivity {
     TextView nav_registration_status_message;
-    String mockStatus = "";
     private ImageView imageViewCheckValidName;
     private EditText searchInput;
     private Button searchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_registration_status);
-
         searchInput = findViewById(R.id.nav_registration_search_name_input);
         searchButton = findViewById(R.id.nav_registration_search_button);
-        
         imageViewCheckValidName = findViewById(R.id.imageView_check_valid_name);
-        if (mockStatus.equals("API return 'Mock Server Error'")) {
-            nav_registration_status_message = (TextView) findViewById(R.id.nav_registration_status_message);
-            nav_registration_status_message.setText("Server Error...");
-        }
-        if (mockStatus.equals("API return 'Student name not existing'")) {
-            nav_registration_status_message.setText("This Student Not Existing");
-        }
+
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String inputName = searchInput.getText().toString();
-                String connectionString = "mongodb://zannatulsakina:gAQEnlXLX8ejm3Rm@ac-fjn97pp-shard-00-00.0vezpan.mongodb.net:27017,ac-fjn97pp-shard-00-01.0vezpan.mongodb.net:27017,ac-fjn97pp-shard-00-02.0vezpan.mongodb.net:27017/?ssl=true&replicaSet=atlas-cyqst2-shard-0&authSource=admin&retryWrites=true&w=majority";
 
+                // Connect to MongoDB Atlas
+                String mongoURI = "mongodb://zannatulsakina:gAQEnlXLX8ejm3Rm@team6centenario.0vezpan.mongodb.net/Team6Centenario";
+                ConnectionString connectionString = new ConnectionString(mongoURI);
                 MongoClientSettings settings = MongoClientSettings.builder()
-                        .applyConnectionString(new ConnectionString(connectionString))
+                        .applyConnectionString(connectionString)
                         .build();
+                MongoClient mongoClient = MongoClients.create(settings);
+                MongoDatabase db = mongoClient.getDatabase(connectionString.getDatabase());
 
-                // Create a new client and connect to the server
-                try (MongoClient mongoClient = MongoClients.create(settings)) {
-                    try {
-                        // Send a ping to confirm a successful connection
-                        MongoDatabase database = mongoClient.getDatabase("admin");
-                        database.runCommand(new Document("ping", 1));
-                        System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-                    } catch (MongoException e) {
-                        nav_registration_status_message = (TextView) findViewById(R.id.nav_registration_status_message);
-                        nav_registration_status_message.setText(e.getMessage());
-                    }
+                // Access to Registration database
+                MongoDatabase database = mongoClient.getDatabase("Registration");
+
+                // Acceses to Student collection
+                MongoCollection<Document> collection = database.getCollection("Student");
+
+                // Check registration status by searching the student name
+                Document student = collection.find(new Document("name", inputName)).first();
+
+                // If student name not found, return not found message
+                if (student == null) {
+                    nav_registration_status_message = (TextView) findViewById(R.id.nav_registration_status_message);
+                    nav_registration_status_message.setText("Student name not found");
                 }
 
-                if (setApiReturnStatus()) {
+                // If student name found, return registration status true or false
+                if (student.getBoolean("registration_status")==true) {
                     imageViewCheckValidName.setImageResource(R.drawable.yes);
                 } else {
                     imageViewCheckValidName.setImageResource(R.drawable.no);
                 }
             }
         });
-    }
-    public boolean setApiReturnStatus() {
-        if(1==1/*Return registration is true*/){
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 }
 
